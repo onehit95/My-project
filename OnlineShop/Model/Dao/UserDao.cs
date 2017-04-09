27bdb722f79deec7ffebe41de48abf7a6389aaeb
Nguyễn Thành Trung <trung.hnue@gmail.com>
2017-04-09 17:07:54 +0700

    update form login

--- OnlineShop/Model/Dao/UserDao.cs
+++ OnlineShop/Model/Dao/UserDao.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Model.EF;
 namespace Model.Dao
 {
     public class UserDao
     {
+        OnlineShopDbContext db = null;
+        public UserDao() {
+            db = new OnlineShopDbContext();
+        }
+
+        public long Insert(User entity) {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public User GetById(string userName) {
+            return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+
+        public bool Login(string userName, string password) {
+            var result = db.Users.Count(x => x.UserName == userName && x.Password == password);
+            if (result > 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
     }
 }
