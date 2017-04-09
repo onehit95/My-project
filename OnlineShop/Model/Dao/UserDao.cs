8253f04bba0e88188cbd0e8d88e0422d33dc215d
Nguyễn Thành Trung <trung.hnue@gmail.com>
2017-04-09 22:36:56 +0700

    upgrade form login

--- OnlineShop/Model/Dao/UserDao.cs
+++ OnlineShop/Model/Dao/UserDao.cs
@@ -23,13 +23,21 @@ namespace Model.Dao
             return db.Users.SingleOrDefault(x => x.UserName == userName);
         }
 
-        public bool Login(string userName, string password) {
-            var result = db.Users.Count(x => x.UserName == userName && x.Password == password);
-            if (result > 0) {
-                return true;
+        public int Login(string userName, string password) {
+            var result = db.Users.SingleOrDefault(x => x.UserName == userName);
+            if (result == null) {
+                return 0;
             }
             else {
-                return false;
+                if (result.Status == false) {
+                    return -1;
+                }
+                else {
+                    if (result.Password == password)
+                        return 1;
+                    else 
+                        return -2;
+                }
             }
         }
 
