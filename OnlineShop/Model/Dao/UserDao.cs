b0d24a3e2867020fe0b65032cbc2b7d0fab02512
Nguyễn Thành Trung <trung.hnue@gmail.com>
2017-04-16 17:34:50 +0700

    update CKEditor

--- OnlineShop/Model/Dao/UserDao.cs
+++ OnlineShop/Model/Dao/UserDao.cs
@@ -35,7 +35,7 @@ namespace Model.Dao
                 db.SaveChanges();
                 return true;
             }
-            catch (Exception ex) {
+            catch (Exception) {
                 return false;
             }
         }
